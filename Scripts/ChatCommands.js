96e92c315c8e5bcda0c180fd77a47182a3ccaf76
Vaugusto <samlara273@gmail.com>
2024-10-21 18:46:07 -0300

    :sparkles: Enhanced meter converter, :construction_worker: add beta NPC Creation menu :sparkles:

--- Scripts/ChatCommands.js
+++ Scripts/ChatCommands.js
@@ -29,6 +29,7 @@ Hooks.on("chatCommandsReady", commands => {
             callback: (chat, parameters, messageData) => {
                 let feets = parameters;
                 let meter = feets * 0.3048;
+                let roundmeters = math.round(meter / 1.5) * 1.5;
                 if (isNaN(feets)){
                     return { 
                         content: "Please, insert a valid number of feets",
@@ -37,7 +38,7 @@ Hooks.on("chatCommandsReady", commands => {
                 }
                 else{
                     return{
-                        content: `<strong>${feets}fts</strong> is approximately <strong>${meter} meters</strong>.`,
+                        content: `<strong>${feets}fts</strong> is approximately <strong>${meter} meters</strong>.<br>rounding by the meters of a common grid (1.5 meters)<strong>${roundmeters}</strong>`,
                         whisper: [game.user.id]
                     }
                 }
@@ -103,6 +104,224 @@ Hooks.on("chatCommandsReady", commands => {
                         whisper: [game.user.id]
                     }
                 }
+            }
+        }
+    );
+    game.chatCommands.register(
+        {
+            name:"/createnpc",
+            module: "UsefulCommands",
+            aliases: ["/cnpc"],
+            description: "Opens a NPC creation dialog",
+            callback: (chat, parameters, messageData) => {
+                async function createNPC(name, race, bio, str, IsSTR, dex, IsDEX, con, IsCON, int, IsINT, wis, IsWIS, cha, IsCHA, ToSummon) {
+                    let npcData = {
+                        name: name,
+                        type: "npc", // Certifique-se de que o tipo esteja correto conforme o sistema que você está usando
+                        img: "icons/svg/mystery-man.svg", // Substitua pelo caminho da imagem do NPC
+                        system: {
+                            abilities: {
+                                str: { value: str, proficient: IsSTR },
+                                dex: { value: dex, proficient: IsDEX },
+                                con: { value: con, proficient: IsCON },
+                                int: { value: int, proficient: IsINT },
+                                wis: { value: wis, proficient: IsWIS },
+                                cha: { value: cha, proficient: IsCHA },
+                        },
+                        details: {
+                            race: race,
+                            biography: { value: bio },
+                        },
+                        currency: {
+                            cp: "",
+                            sp: "",
+                            gp: "",
+                            pp: "",
+                        },
+                        skills: {},
+                        },
+                    };
+                    await Actor.create(npcData);
+                    let actorId = newActor.id;
+                    ui.notifications.info(`NPC "${name}" criado com sucesso!`);
+                    ui.notifications.warn(ToSummon + " e " + actorId);
+                    if (ToSummon == 1){
+                            Summon(name, actorId)
+                        }
+                    }
+                    function openAttributesDialog(name, race, bio) {
+                    new Dialog({
+                        title: "Atributos do NPC",
+                        content: `
+                            <style>
+                            .npc-dialog .form-group {
+                                margin-bottom: 10px;
+                            }
+                            .npc-dialog label {
+                                font-weight: bold;
+                            }
+                            .npc-dialog input[type="number"] {
+                                width: 100%;
+                                padding: 5px;
+                                box-sizing: border-box;
+                            }
+                            </style>
+                            <form class="npc-dialog">
+                            <div class="form-group">
+                                <label>Força:</label>
+                                <input type="number" min="1" max="20" value="10" name="npcSTR" />
+                                <input type="checkbox" name="IsSTR"/>
+                            </div>
+                            <div class="form-group">
+                                <label>Destreza:</label>
+                                <input type="number" min="1" max="20" value="10" name="npcDEX" />
+                                <input type="checkbox" name="IsDEX"/>
+                            </div>
+                            <div class="form-group">
+                                <label>Constituição:</label>
+                                <input type="number" min="1" max="20" value="10" name="npcCON" />
+                                <input type="checkbox" name="IsCON"/>
+                            </div>
+                            <div class="form-group">
+                                <label>Inteligência:</label>
+                                <input type="number" min="1" max="20" value="10" name="npcINT" />
+                                <input type="checkbox" name="IsINT"/>
+                            </div>
+                            <div class="form-group">
+                                <label>Sabedoria:</label>
+                                <input type="number" min="1" max="20" value="10" name="npcWIS" />
+                                <input type="checkbox" name="IsWIS"/>
+                            </div>
+                            <div class="form-group">
+                                <label>Carisma:</label>
+                                <input type="number" min="1" max="20" value="10" name="npcCHA" />
+                                <input type="checkbox" name="IsCHA"/>
+                            </div>
+                            <div class="form-group">
+                                <label>Summonar?</label>
+                                <input type="checkbox" name="Summon"/>
+                            </div>
+                            </form>
+                        `,
+                        buttons: {
+                        ok: {
+                            label: "OK",
+                            callback: async (html) => {
+                            // Obtém os atributos do NPC da entrada do usuário
+                            let npcSTR = parseInt(html.find('[name="npcSTR"]').val());
+                            let IsSTR = parseInt(html.find('[name="IsSTR"]').is(":checked") ? 1 : 0);
+                            let npcDEX = parseInt(html.find('[name="npcDEX"]').val());
+                            let IsDEX = parseInt(html.find('[name="IsDEX"]').is(":checked") ? 1 : 0);
+                            let npcCON = parseInt(html.find('[name="npcCON"]').val());
+                            let IsCON = parseInt(html.find('[name="IsCON"]').is(":checked") ? 1 : 0);
+                            let npcINT = parseInt(html.find('[name="npcINT"]').val());
+                            let IsINT = parseInt(html.find('[name="IsINT"]').is(":checked") ? 1 : 0);
+                            let npcWIS = parseInt(html.find('[name="npcWIS"]').val());
+                            let IsWIS = parseInt(html.find('[name="IsWIS"]').is(":checked") ? 1 : 0);
+                            let npcCHA = parseInt(html.find('[name="npcCHA"]').val());
+                            let IsCHA = parseInt(html.find('[name="IsCHA"]').is(":checked") ? 1 : 0);
+                            let ToSummon = parseInt(html.find('[name="Summon"]').is(":checked") ? 1 : 0);
+                            // Cria o NPC com o nome, raça e atributos fornecidos
+                            console.log(IsCON)
+                            await createNPC(
+                                name,
+                                race,
+                                bio,
+                                npcSTR,
+                                IsSTR,
+                                npcDEX,
+                                IsDEX,
+                                npcCON,
+                                IsCON,
+                                npcINT,
+                                IsINT,
+                                npcWIS,
+                                IsWIS,
+                                npcCHA,
+                                IsCHA,
+                                ToSummon
+                            );
+                        },
+                    },
+                        cancel: {
+                            label: "Cancelar",
+                        },
+                    },
+                        default: "ok",
+                    }).render(true);
+                }
+                    new Dialog({
+                    title: "Criar NPC",
+                    content: `
+                        <style>
+                            .npc-dialog .form-group {
+                                margin-bottom: 10px;
+                            }
+                            .npc-dialog label {
+                                font-weight: bold;
+                            }
+                            .npc-dialog input[type="text"] {
+                                width: 100%;
+                                padding: 5px;
+                                box-sizing: border-box;
+                            }
+                        </style>
+                        <form class="npc-dialog">
+                            <div class="form-group">
+                                <label>Nome do NPC:</label>
+                                <input type="text" name="npcName" />
+                            </div>
+                            <div class="form-group">
+                                <label>Raça do NPC:</label>
+                                <input type="text" name="npcRace" />
+                            </div>
+                            <div class="form-group">
+                                <label>Biografia:</label>
+                                <textarea name="npcBio" rows="4"></textarea>
+                            </div>
+                        </form>
+                    `,
+                    buttons: {
+                    ok: {
+                        label: "OK",
+                        callback: (html) => {
+                          // Obtém o nome do NPC da entrada do usuário
+                            let npcName = html.find('[name="npcName"]').val();
+                            let npcRace = html.find('[name="npcRace"]').val();
+                            let npcBio = html.find('[name="npcBio"]').val();
+                            if (npcName) {
+                            // Abre o segundo diálogo para pedir a raça do NPC
+                            openAttributesDialog(npcName,npcRace,npcBio);
+                            } else {
+                                ui.notifications.warn("Por favor, insira um nome para o NPC.");
+                            }
+                        },
+                        },
+                        cancel: {
+                            label: "Cancelar",
+                        },
+                    },
+                    default: "ok",
+                }).render(true);
+                async function Summon(name, aId){
+                        let position = await warpgate.crosshairs.show({
+                            size:1,
+                            icon: 'icons/magic/movement/trail-streak-zigzag-yellow.webp',
+                            label: 'Summon NPC',
+                            tag: 'Summon NPC',
+                            drawIcon: true,
+                            drawOutline: true,
+                            interval: 0
+                        })
+                        let tokenData = {
+                        name: name,
+                        img: actor.prototypeToken.texture.src,
+                        actorId: aId,
+                        x: position.x,
+                        y: position.y
+                    };
+                await TokenDocument.create(tokenData, { parent: scene });
+            }
             }
         }
     );
